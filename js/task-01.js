ff597557f04c0dd28bc6575e7e4a6f69857b03a1
Danil Mishchenko <dan5679mi@gmail.com>
2021-04-11 01:09:13 +0300

    Решение задачи

---
+++ js/task-01.js
@@ -0,0 +1,7 @@
+const listCategoriesEl = document.querySelectorAll('.item');
+console.log(`В списке ${listCategoriesEl.length} категорий`);
+
+listCategoriesEl.forEach(item => {
+    console.log(`Категория: ${item.firstElementChild.textContent}`);
+    console.log(`Количество элементов: ${item.querySelectorAll('li').length}`);
+});
